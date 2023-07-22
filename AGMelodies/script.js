e54f321958d2fe046faaaaa208549a1bbd69911d
AYUSH GUPTA <118110120+the-ayushgupta@users.noreply.github.com>
2023-07-22 07:08:10 +0530

    Update script.js

--- AGMelodies/script.js
+++ AGMelodies/script.js
@@ -1,1 +1,35 @@
+console.log("Welcome to AGMelodies");
+
+//Initialize The Varibles
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgessBar = document.getElementById('myProgessBar');
+let songs = 
+[
+    {SongName: "Tere Vaste", filePath: "Songs/1.mp3", coverPath: "covers/1.jpg"},
+    {SongName: "Tere Vaste", filePath: "Songs/1.mp3", coverPath: "covers/1.jpg"},
+    {SongName: "Tere Vaste", filePath: "Songs/1.mp3", coverPath: "covers/1.jpg"},
+    {SongName: "Tere Vaste", filePath: "Songs/1.mp3", coverPath: "covers/1.jpg"},
+    {SongName: "Tere Vaste", filePath: "Songs/1.mp3", coverPath: "covers/1.jpg"},
+    {SongName: "Tere Vaste", filePath: "Songs/1.mp3", coverPath: "covers/1.jpg"},
+    {SongName: "Tere Vaste", filePath: "Songs/1.mp3", coverPath: "covers/1.jpg"},
+    {SongName: "Tere Vaste", filePath: "Songs/1.mp3", coverPath: "covers/1.jpg"},
+    {SongName: "Tere Vaste", filePath: "Songs/1.mp3", coverPath: "covers/1.jpg"},
+    {SongName: "Tere Vaste", filePath: "Songs/1.mp3", coverPath: "covers/1.jpg"},
+];
+
+//Handle play/pause
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0)
+    {
+        audioElement.play();
+        masterPlay.classList.remove('fa-solid fa-circle-play fa-beat');
+        masterPlay.classList.add('fa-solid fa-circle-pause fa-beat');
+    }
+})
+
+myProgessBar.document.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate');
+});
 
